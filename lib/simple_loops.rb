e2101e70b2ab88735fa0276cc2a4e88fbfcb189d
Hailey Ringier <haileyringier@outlook.com>
2020-06-12 22:16:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,4 +13,15 @@ def loop_message_n_times (string, n)
     counter += 1 
   end
 end 
+
+def output_array (array)
+  counter = 0 
+  while counter < array.length do
+    puts array
+    counter += 1 
+  end
+end
+
+#def return_string_array (array)
+  
     
